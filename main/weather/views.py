f5b4ef7e0dd25d88198b7c98e4d55a46fb038ab1
SRIJAN CHAKRABORTY <srijanch0810@gmail.com>
2020-07-07 02:52:37 +0530

    updated home.css and removed background image to make it authentic

--- main/weather/views.py
+++ main/weather/views.py
@@ -13,8 +13,8 @@ def home(request):
 	return render(request, 'home.html')
 
 def register(request):
-	form = UserCreationForm()
 	if request.method =='GET':
+		form = UserCreationForm()
 		return render(request,'register.html',{'form': form} )
 	else:
 		if request.POST['password1']==request.POST['password2']:
